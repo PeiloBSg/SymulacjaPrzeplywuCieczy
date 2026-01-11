130252f9b6338c3bc567d5203524172b9231d07a
Paweł Rogula <pawelrogula10@gmail.com>
2026-01-11 14:27:27 +0100

    Optymalizacja kodu
    Zoptymalizowałem plik symulacja_ui.py, poprzez dodanie plików:
    - uklad.py ( w którym tworzone są zbiorniki, rury, pompy oraz grzałka )
    - sterownik.py ( który odpowiedzialny jest za proces automatyki w programie)

---
+++ SymulacjaPrzepływuCieczy/sterownik.py
@@ -0,0 +1,111 @@
+﻿class Sterownik:
+    def __init__(self, uklad_fizyczny):
+        # Sterownik musi mieć dostęp do sprzętu (pomp, zaworów, zbiorników)
+        self.uklad = uklad_fizyczny
+        self.tempo = 0.8  # Szybkość symulacji
+
+    def wykonaj_cykl(self):
+        """
+        Ta metoda wykonuje jeden krok logiki automatyki.
+        Jest wywoływana przez Timer z głównego okna.
+        """
+        u = self.uklad # Alias dla wygody, żeby nie pisać self.uklad.z1...
+        
+        # 1. Z1 -> Z2 (Transfer)
+        if not u.z1.czy_pusty() and not u.z2.czy_pelny():
+            u.p1.wlacz() 
+            # Sprawdzenie fizyczne pompy (czy włącznik jest ON)
+            if u.p1.is_running:
+                ilosc = u.z1.usun_ciecz(self.tempo)
+                u.z2.dodaj_ciecz(ilosc)
+                u.rura1.ustaw_przeplyw(True)
+            else:
+                u.rura1.ustaw_przeplyw(False)
+        else:
+            u.p1.wylacz()
+            u.rura1.ustaw_przeplyw(False)
+
+        # 2. Z2 -> Z3 (Dozowanie)
+        if u.z2.poziom > 0.05 and not u.z3.czy_pelny():
+            ilosc = u.z2.usun_ciecz(self.tempo * 0.8)
+            u.z3.dodaj_ciecz(ilosc)
+            u.rura2.ustaw_przeplyw(True)
+        else:
+            u.rura2.ustaw_przeplyw(False)
+
+        # 3. Reaktor Z3 (Grzanie / Reakcja)
+        if u.z3.aktualna_ilosc > 10:
+            u.grzalka.ustaw_stan(True) 
+            # Sprawdzenie fizyczne grzałki (bezpiecznik)
+            if u.grzalka.is_active:
+                u.z3.ogrzej(0.4)
+            else:
+                u.z3.schlodz(0.1)
+        else:
+            u.grzalka.ustaw_stan(False)
+            u.z3.schlodz(0.1)
+
+        # 4. Z3 -> P2 -> Z4/Z5 (Spust do chłodnic)
+        gotowy_do_spustu = (u.z3.temperatura > 55.0 or u.z3.poziom > 0.85)
+        miejsce_w_chlodnicach = (not u.z4.czy_pelny() or not u.z5.czy_pelny())
+
+        if gotowy_do_spustu and miejsce_w_chlodnicach and u.z3.aktualna_ilosc > 0:
+            u.p2.wlacz()
+            if u.p2.is_running:
+                ilosc = u.z3.usun_ciecz(self.tempo * 2.0)
+                polowa = ilosc / 2.0
+                r5a = False
+                r5b = False
+
+                # Rozdział na dwie chłodnice
+                if not u.z4.czy_pelny():
+                    u.z4.dodaj_ciecz(polowa)
+                    r5a = True
+                else:
+                    u.z5.dodaj_ciecz(polowa)
+
+                if not u.z5.czy_pelny():
+                    u.z5.dodaj_ciecz(polowa)
+                    r5b = True
+                else:
+                    u.z4.dodaj_ciecz(polowa)
+
+                u.rura3.ustaw_przeplyw(True)
+                u.rura4.ustaw_przeplyw(True)
+                u.rura5a.ustaw_przeplyw(r5a)
+                u.rura5b.ustaw_przeplyw(r5b)
+            else:
+                u.rura3.ustaw_przeplyw(False)
+                u.rura4.ustaw_przeplyw(False)
+                u.rura5a.ustaw_przeplyw(False)
+                u.rura5b.ustaw_przeplyw(False)
+        else:
+            u.p2.wylacz()
+            u.rura3.ustaw_przeplyw(False)
+            u.rura4.ustaw_przeplyw(False)
+            u.rura5a.ustaw_przeplyw(False)
+            u.rura5b.ustaw_przeplyw(False)
+
+        # 5. Powrót Z4/Z5 -> Z1
+        # Z4
+        if u.z4.poziom > 0.05:
+            u.z4.schlodz(0.6)
+            ilosc = u.z4.usun_ciecz(self.tempo * 0.6)
+            u.z1.dodaj_ciecz(ilosc)
+            u.rura6.ustaw_przeplyw(True)
+        else:
+            u.rura6.ustaw_przeplyw(False)
+
+        # Z5
+        if u.z5.poziom > 0.05:
+            u.z5.schlodz(0.6)
+            ilosc = u.z5.usun_ciecz(self.tempo * 0.6)
+            u.z1.dodaj_ciecz(ilosc)
+            u.rura7.ustaw_przeplyw(True)
+            u.rura6.ustaw_przeplyw(True) # Wspólny kolektor
+        else:
+            u.rura7.ustaw_przeplyw(False)
+
+        # Fizyka otoczenia (naturalne chłodzenie)
+        u.z1.schlodz(0.02)
+        u.z2.schlodz(0.02)
---
+++ SymulacjaPrzepływuCieczy/uklad.py
@@ -0,0 +1,97 @@
+﻿from zbiornik import Zbiornik
+from rura import Rura
+from pompa import Pompa
+from grzalka import Grzalka
+
+class Uklad:
+    def __init__(self):
+        # === STAŁE GEOMETRYCZNE ===
+        self.X_AXIS_LEFT = 140
+        self.X_AXIS_RIGHT = 760
+        self.Y_Z2 = 50
+        self.Y_Z3 = 50
+        self.Y_P1 = 300
+        self.Y_P2 = 290
+        self.Y_SPLIT = 340
+        self.Y_Z4_Z5 = 380
+        self.Y_BOTTOM_RAIL = 660
+
+        # Listy do łatwego rysowania
+        self.lista_zbiornikow = []
+        self.lista_rur = []
+        self.lista_urzadzen = [] # Pompy, grzałki
+
+        # Inicjalizacja komponentów
+        self._utworz_zbiorniki()
+        self._utworz_urzadzenia()
+        self._utworz_rury()
+
+    def _utworz_zbiorniki(self):
+        # Z1: Bufor
+        self.z1 = Zbiornik(self.X_AXIS_LEFT - 90, 500, 180, 120, "Z1: Bufor", pojemnosc=300.0)
+        self.z1.aktualna_ilosc = 200.0
+        self.z1.aktualizuj_poziom()
+
+        # Z2: Dozownik
+        self.z2 = Zbiornik(self.X_AXIS_LEFT - 90, self.Y_Z2, 180, 120, "Z2: Dozownik")
+
+        # Z3: Reaktor
+        self.z3 = Zbiornik(self.X_AXIS_RIGHT - 110, self.Y_Z3, 220, 200, "Z3: Reaktor", pojemnosc=400.0)
+
+        # Z4 i Z5: Chłodnice
+        OFFSET = 150
+        self.z4 = Zbiornik(self.X_AXIS_RIGHT - OFFSET - 60, self.Y_Z4_Z5, 120, 120, "Z4: Chłodnica 1")
+        self.z5 = Zbiornik(self.X_AXIS_RIGHT + OFFSET - 60, self.Y_Z4_Z5, 120, 120, "Z5: Chłodnica 2")
+
+        # Dodajemy do listy
+        self.lista_zbiornikow = [self.z1, self.z2, self.z3, self.z4, self.z5]
+
+    def _utworz_urzadzenia(self):
+        self.p1 = Pompa(self.X_AXIS_LEFT, self.Y_P1)
+        self.p2 = Pompa(self.X_AXIS_RIGHT, self.Y_P2)
+        self.grzalka = Grzalka(self.X_AXIS_RIGHT - 60, self.Y_Z3 + 150, 120, 30)
+
+        # Lista pomocnicza (opcjonalnie do rysowania pętli)
+        self.lista_urzadzen = [self.p1, self.p2, self.grzalka]
+
+    def _utworz_rury(self):
+        # Rura 1: Z1 -> P1 -> Z2
+        pts_r1 = [self.z1.punkt_gora(), (self.X_AXIS_LEFT, self.Y_P1), self.z2.punkt_dol()]
+        self.rura1 = Rura(pts_r1)
+
+        # Rura 2: Z2 -> Z3
+        y_r2 = self.Y_Z2 + 60
+        pts_r2 = [(self.z2.x + self.z2.width, y_r2), (self.z3.x, y_r2)]
+        self.rura2 = Rura(pts_r2)
+
+        # Rura 3: Z3 -> P2
+        pts_r3 = [self.z3.punkt_dol(), (self.X_AXIS_RIGHT, self.Y_P2)]
+        self.rura3 = Rura(pts_r3)
+
+        # Rura 4: P2 -> Rozgałęzienie
+        pts_r4 = [(self.X_AXIS_RIGHT, self.Y_P2), (self.X_AXIS_RIGHT, self.Y_SPLIT)]
+        self.rura4 = Rura(pts_r4)
+
+        # Rura 5a: Rozgałęzienie -> Z4
+        x_z4 = self.z4.x + self.z4.width / 2
+        pts_r5a = [(self.X_AXIS_RIGHT, self.Y_SPLIT), (x_z4, self.Y_SPLIT), (x_z4, self.Y_Z4_Z5)]
+        self.rura5a = Rura(pts_r5a)
+
+        # Rura 5b: Rozgałęzienie -> Z5
+        x_z5 = self.z5.x + self.z5.width / 2
+        pts_r5b = [(self.X_AXIS_RIGHT, self.Y_SPLIT), (x_z5, self.Y_SPLIT), (x_z5, self.Y_Z4_Z5)]
+        self.rura5b = Rura(pts_r5b)
+
+        # Rura 6: Z4 -> Powrót
+        y_out = self.Y_Z4_Z5 + 120
+        pts_r6 = [(x_z4, y_out), (x_z4, self.Y_BOTTOM_RAIL), 
+                  (self.X_AXIS_LEFT, self.Y_BOTTOM_RAIL), (self.X_AXIS_LEFT, 500 + 120)]
+        self.rura6 = Rura(pts_r6)
+
+        # Rura 7: Z5 -> Powrót
+        pts_r7 = [(x_z5, y_out), (x_z5, self.Y_BOTTOM_RAIL), (x_z4, self.Y_BOTTOM_RAIL)]
+        self.rura7 = Rura(pts_r7)
+
+        # Lista rur
+        self.lista_rur = [self.rura1, self.rura2, self.rura3, self.rura4, 
+                          self.rura5a, self.rura5b, self.rura6, self.rura7]
